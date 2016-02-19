6dc787b7b4dd033b0be421fca61ee8dea46babad
ycewe <theericewe@live.com>
2016-02-20 04:12:43 +0800

    semi-functional GUI

--- CataList/src/UI/ListInterfaceController.java
+++ CataList/src/UI/ListInterfaceController.java
@@ -1,50 +1,105 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package ListInterface;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.cell.CheckBoxListCell;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.CheckBox;
+import javafx.fxml.Initializable;
+import java.util.ResourceBundle;
+import java.net.URL;
+import javafx.beans.value.ChangeListener;
+import java.lang.String;
 
 /**
  *
  * @author ericewe
  */
-public class ListInterfaceController {
+
+public class ListInterfaceController implements Initializable {
     @FXML private Text feedback;
     @FXML private TextField userInput;
     @FXML private ListView todoList;
-    @FXML private ListView todoClass;
+    @FXML private ListView<String> todoClass;
     
-    public static final ObservableList tasks = 
-        FXCollections.observableArrayList();
-    public static final ObservableList classes = 
-        FXCollections.observableArrayList();
+    public static final ObservableList tasks =
+            FXCollections.observableArrayList();
+    public static final ObservableList classes =
+            FXCollections.observableArrayList();
+    public static final ObservableList completed =
+            FXCollections.observableArrayList();
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        todoClass.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                // Your action here
+                feedback.setText(newValue + " selected");
+            }});
+    }
+    
+    /**
+    @FXML protected void handleMouseClick (MouseEvent e) {
+       feedback.setText(e.toString());
+            tasks.removeAll();
+            todoList.setItems(completed);
+        
+    }
+    **/
     
     @FXML protected void handleSubmitButtonAction(ActionEvent event) {
         feedback.setText("Input entered");
         
-        tasks.add(userInput.getText());
-        todoList.setItems(tasks);
+        String command = userInput.getText();
         
-        
-        if(classes.isEmpty()) {
-            classes.add("Today");
-            todoClass.setItems(classes);
+        /** temp parser **/
+        if(command.toLowerCase().equals("today") && !tasks.isEmpty()) {
+            completed.removeAll();
+            todoList.setItems(tasks);
+        } else if(command.toLowerCase().equals("completed") && !completed.isEmpty()) {
+            tasks.removeAll();
+            todoList.setItems(completed);
+        } else {
+            CheckBox task = new CheckBox(userInput.getText());
+            task.setOnAction(e -> handleCheckedBox(task));
+            
+            tasks.add(task);
+            todoList.setItems(tasks);
+            
+            if(classes.isEmpty()) {
+                classes.add("Today");
+                todoClass.setItems(classes);
+            }
         }
-
+        /*****************/
+        
         userInput.clear();
     }
-
+    
+    private void handleCheckedBox(CheckBox task) {
+        if(task.isSelected()) {
+            if(completed.isEmpty()) {
+                classes.add("Complete");
+            }
+            completed.add(task);
+            tasks.remove(task);
+        }
+        
+        if(!task.isSelected()) {
+            completed.remove(task);
+            tasks.add(task);
+        }
+    }
 }
