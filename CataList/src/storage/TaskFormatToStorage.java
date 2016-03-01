8af3959432408b0ee9d9c85cc4b3b12cf47375db
draslash <A0123977@u.nus.edu>
2016-03-01 17:19:44 +0800

    added delete and display

--- CataList/src/storage/TaskFormatToStorage.java
+++ CataList/src/storage/TaskFormatToStorage.java
@@ -1,12 +1,17 @@
+package storage;
 
 import logic.Task;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
+import org.jdom2.util.IteratorIterable;
 
 public class TaskFormatToStorage {
 	
@@ -39,6 +44,44 @@ public class TaskFormatToStorage {
 		return taskObj.get_messageToUser();
 	}
 	
+	public static String deleteFromStorage(Task taskObj){
+			
+		 List<Element> taskChildren = new ArrayList<Element>();
+	        for (int i = 0; i < index; i++) {
+	            Element taskChild = taskChildren.get(i);
+	            
+	            taskChild.detach();
+	        }
+	        return taskObj.get_messageToUser();
+	}
+	
+	public static boolean displayFromStorage(){
+		
+		  try {  
+			   // converted file to document object  
+			   Document document = saxBuilder.build(file);  
+			     
+			   // get root node from xml  
+			   Element rootNode = document.getRootElement();  
+			     
+			   // got all xml elements(tasks) into a list  
+			      List<Task> taskList = rootNode.getChildren(ELEMENT_TASK);  
+			        
+			      for(int i=0; i<taskList.size(); i++){  
+			       Element element = taskList.get(i);  
+			       System.out.println(element);
+			      }  
+			      return true;
+			       
+			  } catch (JDOMException e) {  
+			   // TODO Auto-generated catch block  
+			   e.printStackTrace();  
+			  } catch (IOException e) {  
+			   // TODO Auto-generated catch block  
+			   e.printStackTrace();  
+			  } 
+	}
+	
 	
 	/* Aaron's previous code
 	public TaskFormatToStorage(Task task){
@@ -59,4 +102,3 @@ public class TaskFormatToStorage {
 	}
 	*/
 }
-	
