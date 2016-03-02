d816a142eeb93fea5e16601f72b4f1aecba8d9e0
pp1mt <pewpew1moartime@gmail.com>
2016-03-02 18:22:03 +0800

    Reworked LogicHandler.
    bugs present. Do not touch.

--- CataList/src/logic/LogicHandler.java
+++ CataList/src/logic/LogicHandler.java
@@ -1,55 +1,59 @@
 package logic;
+
 import java.util.Stack;
 import parser.Parser;
 
 
+
 public class LogicHandler {
+	private static final int INPUT_COMMAND_INDEX = 0;
+	private static final int INPUT_EVENT_INDEX = 1;
 	
-	//TODO:
-	//1> gets called by UI
-	//2> receives string? from UI
-	//3> calls parser
-	//4> receive result from parser
-	//5> create Task objects from results
-	//6> pass to Storage
+	private static final int INPUT_LENGTH_NODATETIME = 1;  
+	private static final int INPUT_LENGTH_NO_TIME_WITH_DATE = 2;  
+	private static final int INPUT_LENGTH_WITH_DATE_TIME = 3;
 	
-	static Stack<String> userInputStack;
-	Parser commandParser;
-	
-	/********Constructor********/
-	public LogicHandler(){
-		this.initialSetup();
-	}
-	
-	private void initialSetup(){
-		userInputStack = new Stack<String>();
-		commandParser = new Parser();
-	}
-	
-	public void handleInput(String userInput){
+	public void processCommand(String userInput){
 	//TODO: parser
+		Parser commandParser = new Parser();
 		String[] formattedString = commandParser(userInput);
 		createTask(formattedString);
 	}
-	/**
-	 * unused
-	private boolean isValidInput(String[] checkString){
-		if(checkString[0] == "invalid"){
-			return false;
+	
+	private Task createTask(String[] userInput){
+		int numberOfFields = userInput.length;
+		if(numberOfFields == 1){
+			createTaskNoDateTime(userInput);
+		} else if ( numberOfFields == 2){
+			
+		} else if ( numberOfFields == 3){
+			
 		} else {
-			return true;
+			return;
 		}
 	}
-	**/
 	
-	private Task createTask(String[] checkString){
-		String commandType = checkString[0];
+	
+	private Task createTaskNoDateTime(String[] checkString){
+		String commandType = checkString[INPUT_COMMAND_INDEX];
+		String userInputEvent = checkString[1];
 		switch(commandType){
 			case "add":
-				return new AddTask(checkString);
+				return new AddTask(userInputEvent);
 			case "delete":
-				return DeleteTask(checkString);
-			
+				return new DeleteTask(userInputEvent);
+			case "clear" :
+				return new ClearTask(userInputEvent);
+			case "display" :
+				return new DisplayTask(userInputEvent);
+			case "edit" :
+				return new EditTask(userInputEvent);
+			case "redo" : 
+				return new RedoTask(userInputEvent);
+			case "undo" :
+				return new UndoTask(userInputEvent);
+			case "invalid":
+				return new InvalidTask(userInputEvent);
 		}
 	}
 	
