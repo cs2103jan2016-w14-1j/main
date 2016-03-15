7364f1d36bd5a2dc0576f0ef55ed7ee065b35a71
ycewe <theericewe@live.com>
2016-03-15 16:11:31 +0800

    logic merge

---
+++ CataList/src/logic/SearchTask.java
@@ -0,0 +1,32 @@
+package logic;
+
+public class SearchTask extends Task {
+
+	private static final String MESSAGE_SEARCH_SUCCESS = "Your search query has returned:";
+	private static final String MESSAGE_SEARCH_FAILURE = "Unable to perform search.";
+	private static final String MESSAGE_SEARCH_EMPTY = "Nothing to return.";
+	private static final String COMMAND_TYPE = "add";
+	
+	private static final boolean IS_MUTATOR = true;
+	
+	//Constructor
+	public SearchTask(String userInput){
+		super(IS_MUTATOR, userInput, COMMAND_TYPE, MESSAGE_SEARCH_SUCCESS);
+	}
+	
+	public SearchTask(String userInput, String date){
+		super(IS_MUTATOR, userInput, COMMAND_TYPE, MESSAGE_SEARCH_SUCCESS, date);
+	}
+	public SearchTask(String userInput, String date, String time){
+		super(IS_MUTATOR,userInput, COMMAND_TYPE, MESSAGE_SEARCH_SUCCESS, time, date);
+	}
+
+	public void setErrorMessageDefault(){
+		this.set_messageToUser(MESSAGE_SEARCH_FAILURE);
+	}
+	
+	public void setErrorMessageEmpty(){
+		this.set_messageToUser(MESSAGE_SEARCH_EMPTY);
+	}
+
+}
---
+++ CataList/src/parser/InvalidInputException.java
@@ -0,0 +1,24 @@
+package parser;
+
+public class InvalidInputException extends Exception{
+	//i honestly have no idea why serialver is needed lolol
+	private static final long serialVersionUID = 1L;
+	private String message = "Invalid input. Please try again.";
+
+	public InvalidInputException() {
+
+	}
+
+	public InvalidInputException(String messageToUser) {
+		this.message = messageToUser;
+	}
+
+	public void setMessage(String newMessage){
+		this.message = newMessage;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	
+}
